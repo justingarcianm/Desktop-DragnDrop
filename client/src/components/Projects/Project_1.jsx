1b0bf79dddd3ef1b296751134e028d0d127faebe
justin <justin@esparza.com>
2022-04-17 19:15:04 -0600

    final touches for now

---
+++ client/src/components/Projects/Project_1.jsx
@@ -0,0 +1,21 @@
+import img from '../../assets/web-design.webp'
+import { FaGithub, FaLink } from 'react-icons/fa'
+
+const ProjectOne = () => {
+    return (
+        <div className="projectSingle">
+            <h3>Project One</h3>
+            <img src={img} alt="projectImage" />
+           <div className="project-content">
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, ad odit reprehenderit esse dignissimos, iure at animi asperiores delectus velit debitis fugiat similique quis itaque nihil officia. Veniam, odit fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, ad odit reprehenderit esse dignissimos, iure at animi asperiores delectus velit debitis fugiat similique quis itaque nihil officia. Veniam, odit fugit?</p>
+            <div className="project-links">
+            <FaGithub/> <FaLink/>
+            </div>
+           </div>
+
+        </div>
+    )
+}
+
+export default ProjectOne
--- client/src/data.js
+++ client/src/data.js
@@ -4,12 +4,15 @@ import chrome from './assets/chrome_icon.png'
 import folder from './assets/folder.png'
 import pdf from './assets/PDF_icon.png'
 import gmail from './assets/gmail_icon.png'
+import avatar from './assets/image_icon.jpg'
 // components
 import { FormSpree } from './components/Form/FormSpree'
 import Resume from '../src/components/Resume/Resume'
 import MyPicture from '../src/components/MyPicture/MyPicture'
 import About from '../src/components/About/About'
-import Projects from '../src/components/Projects/Projects'
+import ProjectOne from '../src/components/Projects/Project_1'
+import ProjectTwo from '../src/components/Projects/Project_2'
+import ProjectThree from '../src/components/Projects/Project_3'
 
 const data = [
     {
@@ -23,19 +26,24 @@ const data = [
         Content:Resume
     },
     {
-        name:'projects',
+        name:'project_one',
         img: folder,
-        Content:Projects
+        Content:ProjectOne
+    },
+    {
+        name:'project_two',
+        img: folder,
+        Content:ProjectTwo
+    },
+    {
+        name:'project_three',
+        img: folder,
+        Content:ProjectThree
     },
     {
         name:'me.png',
-        img: folder,
+        img: avatar,
         Content:MyPicture
-    },
-    {
-        name:'unfinished',
-        img: folder,
-        Content:'unfinished folder'
     },
     {
         name:'email me',
